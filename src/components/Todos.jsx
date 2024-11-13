3c095cf2103c395df5bcdcb06897d2736061450a
Siddique Ahmed <developersiddique123@gmail.com>
2024-11-13 22:36:33 +0600

    starting practice

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Todos.css";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const Todos = () => {
+  // input state
   const [input, setInput] = useState({
     title: "",
     type: "",
   });
+  // todos state
+  const [todos, setTodo] = useState([]);
+
+  //pending todo state
+  const [pendingTodo, setPendingTodo] = useState([])
 
   // input value
   const handleInput = (e) => {
@@ -14,6 +22,51 @@ const Todos = () => {
       [e.target.name]: e.target.value,
     }));
   };
+
+  // create todos
+  const createTodo = async (e) => {
+    e.preventDefault();
+
+    if (!input.title || !input.type) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "All fields are required!",
+      });
+    } else {
+      await axios.post("http://localhost:7070/todos", input);
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Todo saved successfully",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setInput({
+        title: "",
+        type: "",
+      });
+    }
+    setTimeout(() => {
+      getTodo()
+    }, 1000);
+  };
+
+  // get todo
+  const getTodo = async () => {
+    const response = await axios.get("http://localhost:7070/todos");
+    setTodo(response.data);
+  };
+
+  // pending todo
+  const pendingTodos =async  () => {
+    const response = await axios.get("http://localhost:7070/todos?type=pending")
+    setPendingTodo(response.data)
+  }
+
+  useEffect(() => {
+    pendingTodos()
+  }, []);
 
   return (
     <>
@@ -25,66 +78,65 @@ const Todos = () => {
         <div className="todos-form ">
           <div className="row justify-content-center">
             <div className="col-lg-4 ">
-              <input
-                onChange={handleInput}
-                name="title"
-                value={input.title}
-                className="form-control mb-2"
-                type="text"
-                placeholder="Type your todo"
-              />
+              <form onSubmit={createTodo}>
+                <input
+                  onChange={handleInput}
+                  name="title"
+                  value={input.title}
+                  className="form-control mb-2"
+                  type="text"
+                  placeholder="Type your todo"
+                />
 
-              <select
-                onChange={handleInput}
-                name="type"
-                className="form-select mb-2"
-                aria-label="Default select example"
-              >
-                <option selected>Open this select menu</option>
-                <option value="Pending">Pending</option>
-                <option value="Completed">Completed</option>
-                <option value="Deleted">Deleted</option>
-              </select>
-              <button className="form-control btn btn-sm btn-info">Add</button>
+                <select
+                  onChange={handleInput}
+                  name="type"
+                  className="form-select mb-2"
+                  aria-label="Default select example"
+                >
+                  <option selected>Open this select menu</option>
+                  <option value="Pending">Pending</option>
+                  <option value="Completed">Completed</option>
+                  <option value="Deleted">Deleted</option>
+                </select>
+                <button
+                  type="submit"
+                  onClick={createTodo}
+                  className="form-control btn btn-sm btn-info"
+                >
+                  Add
+                </button>
+              </form>
             </div>
           </div>
         </div>
         <hr />
         <div className="todos">
           <div className="row">
-            <div className="col-lg-4 text-center mt-5">
+            <div className="col-lg-4 text-center">
               <h1>hello</h1>
             </div>
-            <div className="col-lg-4 text-center mt-5">
+            <div className="col-lg-4 text-center ">
               <div className="pending-todos">
                 <h2 className=" d-inline-block p-3">Pending</h2>
                 <hr />
                 <ul>
-                  <li className="d-flex justify-content-between bg-warning p-2 align-items-center mb-2">
-                    {" "}
-                    Go to school{" "}
-                    <button className="btn btn-sm btn-success">
-                      Complete{" "}
-                    </button>
-                  </li>
-                  <li className="d-flex justify-content-between bg-warning p-2 align-items-center mb-2">
-                    {" "}
-                    Go to school{" "}
-                    <button className="btn btn-sm btn-success">
-                      Complete{" "}
-                    </button>
-                  </li>
-                  <li className="d-flex justify-content-between bg-warning p-2 align-items-center mb-2">
-                    {" "}
-                    Go to school{" "}
-                    <button className="btn btn-sm btn-success">
-                      Complete{" "}
-                    </button>
-                  </li>
+                  {pendingTodo?.length > 0 ? (
+                    pendingTodo.map((item, index) => (
+                      <li className="d-flex justify-content-between bg-warning p-2 align-items-center mb-2">
+                     {index + 1}. {item.title}
+                      <button className="btn btn-sm btn-success">
+                        Complete
+                      </button>
+                    </li>
+                    ))
+                  ) : (
+                    <li>No todos found</li>
+                  )}
                 </ul>
               </div>
             </div>
-            <div className="col-lg-4 text-center mt-5">
+            <div className="col-lg-4 text-center">
               <h1>hello</h1>
             </div>
           </div>
